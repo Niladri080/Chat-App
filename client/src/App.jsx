209b1daf27f9e06e24eebde6d31a9f50ce982dc7
Niladri080 <nilmandal080@gmail.com>
2025-06-26 22:01:00 +0530

    server.js updated for vercel

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ import {Toaster} from "react-hot-toast";
 import { AuthContext } from "../context/AuthContext";
 const App= ()=>{
   const {authUser} = useContext(AuthContext)
-  return <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain bg-center">
+  return <div className="bg-[url('/bgImage.svg')] bg-contain bg-center">
     <Toaster/>
     <Routes>
       <Route path='/' element={authUser ? <HomePage/> : <Navigate to="/login"/>}/>
--- server/server.js
+++ server/server.js
@@ -49,8 +49,11 @@ app.use("/api/messages", messageRouter);
 
 // DB connection
 await connectDB();
-
-const PORT = process.env.PORT || 6500;
-server.listen(PORT, () => {
-  console.log("Server is running on PORT " + PORT);
-});
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 6500;
+  server.listen(PORT, () => {
+    console.log("Server is running on PORT " + PORT);
+  });
+}
+// Export server for vercel
+export default server;
